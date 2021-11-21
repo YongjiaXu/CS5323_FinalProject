fbd75a2d6b589075b5b6e252858e4f5dce58bba7
YongjiaXu <yongjiax@smu.edu>
2021-11-21 16:52:01 -0600

    add app

---
+++ Relax Your Neck/Relax Your Neck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Relax Your Neck
+//
+//  Created by Yongjia Xu on 11/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
